4deab95bfe2c89f16e57e81800c3010b32251e80
unknown <svetlichnaya.org@gmail.com>
2022-12-05 21:56:06 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,51 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 7, 9, 12, 15};
+        int key = 9;
+        System.out.println(binarySearch(arr, key, 0, arr.length - 1));
+
+        System.out.println();
+        int count = 3;
+        moveRing('A', 'B', 'C', count);
+    }
+
+    static int binarySearch(int[] arr, int key, int start, int end) {
+        int middle = (end + start) / 2;
+        if (end < start) return -1;
+        if (key < arr[middle]) return binarySearch(arr, key, start, end - 1);
+        if (key > arr[middle]) return binarySearch(arr, key, middle + 1, end);
+        if (key == arr[middle]) return middle;
+        return -1;
+    }
+
+//    процедура "Перенести" (A, B, C, N)
+//    начало
+//    если N=1
+//    // Если диск всего один, то надо его перенести напрямую
+//    то
+//    снять диск со стержня A и положить на стержень C;
+//    возврат из процедуры;
+//    иначе
+//        // Переносим все диски, кроме самого большога со стежня
+//        // A на стержень B
+//    Перенести (A,C,B,N-1);
+//    // Переносим самый большой диск со стержня A на стержень C
+//    снять диск со стержня A и положить на стержень C;
+//    // Переносим все диски со стержня B на стержень C поверх
+//    // самого большого диска
+//    Перенести (B,A,C,N-1);
+//    возврат из процедуры;
+//    конец если;
+//    конец процедуры "Перенести";
+
+    public static void moveRing(char a, char b, char c, int count) {
+
+        if (count == 1) {
+            System.out.println(String.format("from %s to %s", a, b));
+        } else {
+            moveRing(a, c, b, count - 1);
+            System.out.println(String.format("from %s to %s", a, b));
+            moveRing(c, b, a, count - 1);
+        }
+    }
+}
